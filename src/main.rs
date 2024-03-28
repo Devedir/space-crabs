32d33cb4d340d1f92d3275d22c19793890a46777
Devedir <109344310+Devedir@users.noreply.github.com>
2024-03-28 13:59:55 +0100

    Basic Rocket app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Crabs can into space!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
